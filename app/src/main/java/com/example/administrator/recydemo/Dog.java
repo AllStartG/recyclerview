25014757ec19054212e0875d64ef4d6edcd8b072
Administrator <843560806@qq.com>
2017-02-21 19:21:50 +0800

    recycler 描述

---
+++ app/src/main/java/com/example/administrator/recydemo/Dog.java
@@ -0,0 +1,14 @@
+package com.example.administrator.recydemo;
+
+/**
+ * Created by Administrator on 2017/2/20.
+ */
+public class Dog {
+    String name;
+    int dogId;
+
+    public Dog(String name, int dogId){
+        this.name = name;
+        this.dogId = dogId;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/recydemo/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.administrator.recydemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private List<Dog> dogList = new ArrayList<Dog>();
+    private MyAdapter mMyAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        initDogList();
+    }
+
+    private void initDogList() {
+        dogList.add(new Dog("dog1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq" , R.drawable.dog1));
+        dogList.add(new Dog("dog2" , R.drawable.dog2));
+        dogList.add(new Dog("dog3" , R.drawable.dog3));
+        dogList.add(new Dog("dog4" , R.drawable.dog4));
+        dogList.add(new Dog("dog5qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq" , R.drawable.dog5));
+        dogList.add(new Dog("dog6" , R.drawable.dog6));
+        dogList.add(new Dog("dog7" , R.drawable.dog7));
+        dogList.add(new Dog("dog8" , R.drawable.dog8));
+        dogList.add(new Dog("dog9" , R.drawable.dog9));
+        dogList.add(new Dog("dog10" , R.drawable.dog10));
+    }
+
+    private void initViews() {
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        // LinearLayoutManager manager = new LinearLayoutManager(this);
+        //LinearLayoutManager manager = new LinearLayoutManager(this);
+        //manager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        //GridLayoutManager manager = new GridLayoutManager(this, 3);
+
+
+        StaggeredGridLayoutManager manager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+
+
+        mRecyclerView.setLayoutManager(manager);
+        mMyAdapter = new MyAdapter(dogList);
+        mRecyclerView.setAdapter(mMyAdapter);
+    }
+}
